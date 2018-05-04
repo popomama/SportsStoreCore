9c004ad9b6d573167e3dd4bc8b788a7630d07aa0
Wei Xia <wei_xia@hotmail.com>
2018-05-04 14:30:07 -0700

    added ProductController

---
+++ SSCore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SSCore.Models;
+
+namespace SSCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Product")]
+    public class ProductController : Controller
+    {
+        IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+    }
+}
